2eb9fcfde46084ccacd81a972fc764e8bbd8d975
Chaojun Zhang <chaojun.zhang@intel.com>
2025-04-21 10:15:41 +0800

    remove assertion (#152)

--- examples/offline_inference/vision_language.py
+++ examples/offline_inference/vision_language.py
@@ -912,7 +912,7 @@ def run_qwen2_vl(questions: list[str], modality: str) -> ModelRequestData:
 # Qwen2.5-VL
 def run_qwen2_5_vl(questions: list[str], modality: str) -> ModelRequestData:
 
-    model_name = "Qwen/Qwen2.5-VL-3B-Instruct"
+    model_name = "Qwen/Qwen2.5-VL-7B-Instruct"
 
     engine_args = EngineArgs(
         model=model_name,
@@ -924,6 +924,8 @@ def run_qwen2_5_vl(questions: list[str], modality: str) -> ModelRequestData:
             "fps": 1,
         },
         limit_mm_per_prompt={"image": 1},
+        enforce_eager=True,
+        disable_mm_preprocessor_cache=args.disable_mm_preprocessor_cache,
     )
 
     if modality == "image":
