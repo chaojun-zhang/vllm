93f8fe8fe5338f287af2c648b2087e435a4db472
chaojun-zhang <chaojun.zhang@intel.com>
2025-11-14 11:14:49 -0800

    fix

--- vllm/lora/ops/ipex_ops/lora_ops.py
+++ vllm/lora/ops/ipex_ops/lora_ops.py
@@ -1,11 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
-# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
-
 import torch
-
-from vllm.logger import init_logger
-
-logger = init_logger(__name__)
+import vllm_xpu_kernels._xpu_C  # noqa: F401
 
 
 def bgmv_shrink(
@@ -16,7 +11,11 @@ def bgmv_shrink(
     scaling: float = 1.0,
 ) -> None:
     torch.ops._xpu_C.bgmv_shrink(
-        output_tensor, inputs, lora_a_weights, lora_indices_tensor, scaling
+        output_tensor,
+        inputs,
+        lora_a_weights,
+        lora_indices_tensor,
+        scaling,
     )
 
 
@@ -26,9 +25,30 @@ def bgmv_expand(
     output_tensor: torch.Tensor,
     lora_indices_tensor: torch.Tensor,
     add_inputs: bool = True,
-) -> None:
+):
+    """
+    Args:
+        inputs (torch.Tensor): Shape: `[batch_size, hidden_size]`.
+        lora_b_weights (torch.Tensor): Shape: `[lora_num, rank, hidden_size]`.
+        output_tensor (torch.Tensor): Shape: `[batch_size, rank]`.
+        lora_indices_tensor (torch.Tensor): Shape: `[batch_size]`.
+         The LoRA index corresponding to each batch. An index of -1 means
+            no lora should be applied.
+        add_inputs (bool, optional):  Defaults to False. adds the final lora
+            results to the output.
+
+    Semantics:
+      for i in range(inputs.size(0)):
+        output_tensor[i] =
+            inputs[i] @ lora_b_weights[lora_indices_tensor[i]]
+            + (inputs[i] if add_inputs else 0)
+    """
     torch.ops._xpu_C.bgmv_expand(
-        output_tensor, inputs, lora_b_weights, lora_indices_tensor, add_inputs
+        output_tensor,
+        inputs,
+        lora_b_weights,
+        lora_indices_tensor,
+        add_inputs,
     )
 
 
@@ -40,15 +60,33 @@ def bgmv_expand_slice(
     slice_offset: int,
     slice_size: int,
     add_inputs: bool = True,
-) -> None:
-    # slice_size only used for validation, no use for kernel
-    assert slice_size == lora_b_weights.size(-2)
-    assert slice_offset + slice_size <= output_tensor.size(1)
+):
+    """
+    Args:
+        inputs (torch.Tensor): Shape: `[batch_size, hidden_size]`.
+        lora_b_weights (torch.Tensor): Shape: `[lora_num, rank, hidden_size]`.
+        output_tensor (torch.Tensor): Shape: `[batch_size, rank]`.
+        lora_indices_tensor (torch.Tensor): Shape: `[batch_size]`.
+            The LoRA index
+            corresponding to each batch. An index of -1 means no lora should be
+            applied.
+        slice_offset (int): output_tensor's offset
+        slice_size (int): current output_tensor's size
+        add_inputs (bool, optional):  Defaults to False. adds the final lora
+            results to the output.
+
+    Semantics:
+      for i in range(inputs.size(0)):
+        output_tensor[i][slice_offset:slice_offset+slice_size] =
+            inputs[i] @ lora_b_weights[lora_indices_tensor[i]]
+            + (inputs[i] if add_inputs else 0)
+    """
     torch.ops._xpu_C.bgmv_expand_slice(
         output_tensor,
         inputs,
         lora_b_weights,
         lora_indices_tensor,
         slice_offset,
+        slice_size,
         add_inputs,
     )
